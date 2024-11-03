cd92efa8036cb39b1227cde34a711315b6a0712a
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2024-11-03 22:19:21 +0600

    Add Home banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 // Home.js
 import React, { useEffect } from "react";
+import banner from "../assets/banner.jpg";
 
 const Home = () => {
   useEffect(() => {
@@ -7,16 +8,27 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <div>
-        <h2>Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h2>
-        <p>
-          Explore the latest gadgets that will take your experience to the next
-          level. From smart devices to the coolest accessories, we have it all!
-        </p>
-        <button className="btn rounded-3xl">Shop Now</button>
+    <div className="bg-[#9538E2] mb-[400px] text-white flex flex-col justify-center items-center text-center py-4 h-[580px] rounded-[32px] relative">
+      <h2 className="text-[56px] font-bold my-4 px-5">
+        Upgrade Your Tech Accessorize with Gadget Heaven Accessories
+      </h2>
+      <p className="text-[16px] py-2">
+        Explore the latest gadgets that will take your experience to the next
+        level. From smart devices to the coolest accessories, we have it all!
+      </p>
+      <button className="btn rounded-3xl">Shop Now</button>
+
+      {/*  */}
+      <div className="absolute left-1/2 transform -translate-x-1/2 top-[450px] bg-[#ffffff65] p-4 rounded-3xl shadow-lg ">
+        <img
+          src={banner}
+          alt="VR Headset"
+          className="rounded-3xl w-[600px] h-[400px]"
+        />
       </div>
     </div>
+    // Now Outlet will here
+  
   );
 };
 
