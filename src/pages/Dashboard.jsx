4b0f12a8f52a02dc73814bffb6b5bb7a91a7466d
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2024-11-04 12:39:59 +0600

    Rewrite again

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -10,7 +10,7 @@ export default function Dashboard() {
 
   return (
     <div className="p-5 text-white bg-[#9538E2] flex flex-col mb-[700px] pt-[50px] pb-[80px] relative items-center justify-center ">
-      <h2 className="text-[32px] font-bold">Product Details</h2>
+      <h2 className="text-[32px] font-bold">Dashboard</h2>
       <p className="max-w-[796px] text-center text-[16px]">
         Explore the latest gadgets that will take your experience to the next
         level. From smart devices to the coolest accessories, we have it all!
