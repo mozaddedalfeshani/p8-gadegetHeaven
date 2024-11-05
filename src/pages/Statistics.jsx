bc6730586df1377dea4b2122175152273356177e
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2024-11-05 19:01:51 +0600

    Add more info to statistics

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -17,6 +17,11 @@ const data = [
   { name: "Apple Watch 6", Price: 60, Total: 55, Rating: 80 },
   { name: "iPhone 15", Price: 110, Total: 95, Rating: 92 },
   { name: "Wireless Charger", Price: 20, Total: 15, Rating: 74 },
+  { name: "Samsung Galaxy S22", Price: 85, Total: 80, Rating: 88 },
+  { name: "Beats Studio Buds", Price: 35, Total: 25, Rating: 82 },
+  { name: "Fitbit Versa 3", Price: 45, Total: 40, Rating: 83 },
+  { name: "Anker PowerPort Wireless", Price: 25, Total: 20, Rating: 76 },
+  { name: "Lenovo Yoga 7i", Price: 95, Total: 90, Rating: 87 },
 ];
 
 export default function Statistics() {
@@ -26,7 +31,7 @@ export default function Statistics() {
 
   return (
     <div>
-      <div className="p-5 text-white bg-[#9538E2] flex flex-col pt-[50px] pb-[80px] relative items-center justify-center ">
+      <div className="p-5 text-white bg-[#9538E2] flex flex-col pt-[50px] pb-[80px] relative items-center justify-center">
         <h2 className="text-[32px] font-bold">Statistics</h2>
         <p className="max-w-[796px] text-center text-[16px]">
           Explore the latest gadgets that will take your experience to the next
@@ -34,18 +39,33 @@ export default function Statistics() {
         </p>
       </div>
 
-      {/* Chart is here astart  */}
+      {/* Chart Start */}
       <div className="container mx-auto my-2 p-5 bg-gray-50 rounded-lg shadow-lg">
         <h2 className="text-2xl font-bold mb-5">Statistics</h2>
         <ResponsiveContainer width="100%" height={400}>
           <ComposedChart data={data}>
-            <XAxis dataKey="name" />
-            <YAxis />
+            <XAxis
+              dataKey="name"
+              label={{ value: "Product", position: "insideBottom", dy: 10 }}
+            />
+            <YAxis
+              label={{
+                value: "Values",
+                angle: -90,
+                position: "insideLeft",
+                dy: -10,
+              }}
+            />
             <Tooltip />
-            <Legend />
-            <Bar dataKey="Price" fill="#A855F7" />
-            <Bar dataKey="Total" fill="#7C3AED" />
-            <Line type="monotone" dataKey="Rating" stroke="#DC2626" />
+            <Legend verticalAlign="top" height={36} />
+            <Bar dataKey="Price" fill="#A855F7" name="Price ($)" />
+            <Bar dataKey="Total" fill="#7C3AED" name="Total Sold" />
+            <Line
+              type="monotone"
+              dataKey="Rating"
+              stroke="#DC2626"
+              name="Rating (%)"
+            />
           </ComposedChart>
         </ResponsiveContainer>
       </div>
